47f8522aebedef74dc9e5f7d81bf4d02a2d5ad82
Lujia Yu <yulujia928@ufl.edu>
2018-03-24 15:36:25 -0400

    UI
    UI sprint2

---
+++ Assets/Scripts/SkeletonButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SkeletonButton : MonoBehaviour {
+
+    public void ButtonAction()
+    {
+        Debug.Log("<b>Switch to Skeleton Mode</b>");
+        SceneManager.LoadScene(0);
+    }
+    
+}
